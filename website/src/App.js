32fab191800cc2b1cdb17d9d253e7e14421cd260
Jcka-Rosy <jrcreations357@gmail.com>
2023-06-22 17:11:56 +0530

    landing page

---
+++ website/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="App-header">
+       <div className="head1">Max Cool Care </div>
+       <div className="head2">Call us at 7400071819</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
